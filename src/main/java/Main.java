7f42ddb32f70dcf10e65437a2067901972551839
Blake Simpson <shadowcloud131@gmail.com>
2021-03-14 13:22:29 -0500

    New student object and comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,49 +7,67 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        ArrayList<Integer> student;
-        List<ArrayList<Integer>> students = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> dorm = null;
+        // student array consisting of compatibility score for each student
+        //ArrayList<Integer> student;
+        Student student;
+        // students array consisting of Student objects
+        List<Student> students = new ArrayList<>();
+        // dorm consisting of 4 students
+        ArrayList<Student> dorm = null;
         // below data structure is smelly
-        List<ArrayList<ArrayList<Integer>>> dorms = new ArrayList<>();
+        // dorms consisting of all dorms
+        List<ArrayList<Student>> dorms = new ArrayList<>();
 
+        // read resource file into student array then add each student to students array
         Scanner scanner = new Scanner(new File("src/main/resources/roommates.txt"));
+        int counterForStudentID = 0;
         while(scanner.hasNext()) {
 
-            student = new ArrayList<>();
+            ArrayList<Integer> compatibilityList = new ArrayList<>();
 
-            while(student.size() < 200) {
+            // each line is 200 characters long not including spaces as they are not counted and instead used as delimiter
+            while(compatibilityList.size() < 200) {
                 // convert next string from scanner to integer and store it in next index of the current student array
-                student.add(Integer.parseInt(scanner.next()));
+                compatibilityList.add(Integer.parseInt(scanner.next()));
             }
-
-            // when the end of a line is reached add the student to the students array
+            // when the end of a line is reached add the compatibilityList to new Student object, increment counter and
+            // add to students list
+            student = new Student(counterForStudentID, compatibilityList);
+            counterForStudentID++;
             students.add(student);
         }
-
         // scanner is closed but it still contains a lot of information. I would kind of like to reclaim the space this
-        // is taking up too
+        // is taking up too - reached out to fellow classmates and they said that the java garbage collection would take
+        // care of this
         scanner.close();
 
         // assign students in intervals of 4 to a room
-        // arbitrary assigning that will be sorted later
+        // sorting by position in list that will be properly sorted later by compatibility
         for (int i = 0; i < students.size(); i++) {
-
-            // if student is an interval of 4 create a new dorm
+            // if student position in array is an interval of 4 create a new dorm
             if (i%4 == 0) {
                 dorm = new ArrayList<>();
             }
-
             // add students to a dorm until its full
             if (dorm.size() < 4) {
                 dorm.add(students.get(i));
             }
-
+            // add dorm to dorms list when it has reached capacity
             if (dorm.size() == 4) {
                 dorms.add(dorm);
             }
         }
 
         // sort the dorms by how compatible students are by using simulated annealing technique
+        // iterate through dorms
+        // for each dorm find total compatibility score
+        // find compatibility score for all students in dorm (AB, AC, AD, BC, BD, CD)
+        
+    }
+
+    // lower compatibility score means greater compatibility
+    double probability(int student1CompScore, int student2CompScore, double temp) {
+        if (student2CompScore > student1CompScore) return 1;
+        return Math.exp((student1CompScore - student2CompScore)/temp);
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,6 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
-    List<Integer> compatibility = new ArrayList<>();
+    ArrayList<Integer> compatibility;
+    int studentID;
 
+    Student(int studentID, ArrayList<Integer> compatibility) {
+        this.compatibility = compatibility;
+        this.studentID = studentID;
+    }
+
+    ArrayList<Integer> getCompatibility() {
+        return compatibility;
+    }
+
+    int getStudentID() {
+        return studentID;
+    }
 }
